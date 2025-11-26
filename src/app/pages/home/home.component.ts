5573ce291822c0107bd8023fd1779ced9d646956
Adevair Dantas <adevairdantas@gmail.com>
2025-11-25 22:27:10 -0300

    Ajustando footer

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header_home/header.component";
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from "./components/footer_home/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, CommonModule],
+  imports: [HeaderComponent, CommonModule, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
