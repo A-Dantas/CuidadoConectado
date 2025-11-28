405a4a3c12dbf1b19003e9c17a44d436e9b76114
PyDantas Dev <adevairdantas@gmail.com>
2025-11-28 11:34:05 -0300

    ajustando background header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './pages/components/about/about.component';
 import { HomeComponent } from './pages/components/home/home.component';
+import { Component } from '@angular/core';
+import { FormsComponent } from './pages/components/forms/forms.component';
 
 export const routes: Routes = [
     {
@@ -13,6 +15,11 @@ export const routes: Routes = [
     {
         path: 'about',
         component: AboutComponent
+    },
+
+    {
+        path: 'forms',
+        component: FormsComponent
     },
 
     // O padrão é que a rota seja "/home", caso aconteça do usuário apagar o '/home' será redirecionado para a /home
--- src/app/pages/components/home/home.component.ts
+++ src/app/pages/components/home/home.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../../shared/header/header.component';
 import { FooterComponent } from '../../../shared/footer_home/footer.component';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, CommonModule, FooterComponent],
+  imports: [HeaderComponent, CommonModule, FooterComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
