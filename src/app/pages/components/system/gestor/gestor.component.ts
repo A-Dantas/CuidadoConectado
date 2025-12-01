30f73644e7cf2010561fe642bc3d61c424dc48cd
Adevair Dantas <adevairdantas@gmail.com>
2025-12-01 07:06:46 -0300

    Iniciando criação dos paineis

--- src/app/pages/components/system/gestor/gestor.component.ts
+++ src/app/pages/components/system/gestor/gestor.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderSystemComponent } from "../shared/header-system/header-system.component";
+import { MenuLateralSystemComponent } from "../shared/menu-lateral-system/menu-lateral-system.component";
+import { HeaderComponent } from "../../../../shared/header/header.component";
 
 @Component({
   selector: 'app-gestor',
-  imports: [HeaderSystemComponent],
+  imports: [HeaderSystemComponent, MenuLateralSystemComponent, HeaderComponent],
   templateUrl: './gestor.component.html',
   styleUrl: './gestor.component.css'
 })
--- src/app/pages/components/system/shared/header-system/header-system.component.ts
+++ src/app/pages/components/system/shared/header-system/header-system.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MenuLateralSystemComponent } from "../menu-lateral-system/menu-lateral-system.component";
 
 @Component({
   selector: 'app-header-system',
-  imports: [RouterModule],
+  imports: [RouterModule, MenuLateralSystemComponent],
   templateUrl: './header-system.component.html',
   styleUrl: './header-system.component.css'
 })
