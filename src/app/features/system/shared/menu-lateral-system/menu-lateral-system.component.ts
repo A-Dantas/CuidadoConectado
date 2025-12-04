5fa98c91d5ec2e32f21c84b310d6e8631db84253
Adevair Dantas <adevairdantas@gmail.com>
2025-12-04 00:36:41 -0300

    Implementando funcionalidade do menu lateral

--- src/app/features/system/shared/menu-lateral-system/menu-lateral-system.component.ts
+++ src/app/features/system/shared/menu-lateral-system/menu-lateral-system.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-menu-lateral-system',
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './menu-lateral-system.component.css'
 })
 export class MenuLateralSystemComponent {
+  menuSelecionado: number = 1;
 
+  @Output() menuSelecionadoChange = new EventEmitter<number>();
+
+  selecionarMenu(numeroMenu: number): void {
+    this.menuSelecionado = numeroMenu;
+    this.menuSelecionadoChange.emit(numeroMenu);
+  }
 }
